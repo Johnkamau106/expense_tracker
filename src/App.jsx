549e557f4a80976897c58ceb5cb9a804d379d5ab
johnkamau106 <john.kamau7@student.moringaschool.com>
2025-04-12 14:58:33 +0300

    added alot of css

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,25 @@ function App() {
     <>
       <header>
         <h1>Express Tracker</h1>
-        <p>Start taking control of your finances and life records, categorize and analyze your spending</p>
+        <p>Start taking control of your finances and life records,<br /> categorize and analyze your spending</p>
       </header>
      
      <section>
-      <h3>Add Expense</h3>
+      <h2> <b>Add Expense</b></h2>
       <p>Enter your expense details below</p>
        <form action="">
-        <input type="text" placeholder="enter exoense name"/> <br />
+        <input type="text" placeholder="enter expense name"/> <br />
         <input type="text" placeholder="enter expense description"/> <br />
         <input type="text" placeholder="enter expense category"/> <br />
-        <input type="text" placeholder="enter expense amount"/> <br />
+        <input type="date" id="dateInput" name="dateInput"/> <br />
         <button>submit</button>
-
-
        </form>
          </section>
+
+         
+          <input type="text" placeholder="search" />
+
+         
       
     </>
   )
