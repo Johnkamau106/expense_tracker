4f7494dd257fa88435c07c53d940865ae573a4f4
johnkamau106 <john.kamau7@student.moringaschool.com>
2025-04-12 17:38:07 +0300

    added more css by positioning the table on the right side

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,63 @@ function App() {
         <input type="text" placeholder="enter expense name"/> <br />
         <input type="text" placeholder="enter expense description"/> <br />
         <input type="text" placeholder="enter expense category"/> <br />
+        <input type="number" placeholder="enter amount"/> <br />
         <input type="date" id="dateInput" name="dateInput"/> <br />
         <button>submit</button>
        </form>
          </section>
 
+         <div class="search-bar">
+          <input type="text" placeholder="search expenses" />
+          </div>
+
+
+         <div class="table-container">
+
          
-          <input type="text" placeholder="search" />
+
+          <table>
+            <tr>
+            <th>Expense</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+            <th>Date</th>
+            </tr>
+            <tr>
+              <td>matumbo</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>matumbo</td>
+              <td>ugali</td>
+              <td></td>
+              <td>mayai</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>matumbo</td>
+              <td>ugali</td>
+              <td></td>
+              <td>mayai</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>matumbo</td>
+              <td>ugali</td>
+              <td></td>
+              <td>mayai</td>
+              <td></td>
+            </tr>
+
+
+          </table>
+
+          </div>
+
 
          
       
